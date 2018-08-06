a06048d7dccb2bc052bef6ff2d6899b5505cb9b4
Micah Yunana Bala <micahkulien@gmail.com>
2018-08-06 10:37:17 +0100

    break code into separate functions

--- js/app.js
+++ js/app.js
@@ -36,7 +36,7 @@ const pictureArr = [
 ];
 
 /*
-+ function to Build Grid of cards dynamically.
+function to Build Grid of cards dynamically.
 */
 function buildGrid() {
   const docFragment = document.createDocumentFragment();
@@ -54,21 +54,57 @@ function buildGrid() {
   }
 
   const cardsFrame = document.querySelector(".card_frame");
-
   cardsFrame.appendChild(docFragment);
 }
 
 // function to display card once clicked
-const open = function() {};
+const open = function(targetCard) {
+  //   wnat to be sure we are clicking on an element that represents a card
+  if (targetCard.nodeName.toLowerCase() === "li") {
+    // want to be sure that the card we are clicking is not having the classes open, show and match
+    if (
+      !targetCard.classList.contains("open") &&
+      !targetCard.classList.contains("show") &&
+      !targetCard.classList.contains("match")
+    ) {
+      // Add clicked card into the openCards array and add classes show, and open to them
+      add(targetCard);
+    }
+  }
+  return targetCard;
+};
 
-// function to add card to a list of opened cards
-const add = function() {};
+// function to add card to an array of opened cards
+const add = function(targetCard) {
+  openCards.push(targetCard);
+  targetCard.classList.add("open", "show");
+  //Make sure we can only have a max of two cards in the array to check for a match
+  if (openCards.length === 2) {
+    if (
+      openCards[0].firstChild.className === openCards[1].firstChild.className
+    ) {
+      // console.log("Match!");
+      match();
+      openCards = [];
+    } else {
+      setTimeout(function() {
+        for (const card of openCards) {
+          card.classList.remove("show", "open");
+        }
+        openCards = [];
+      }, 500);
+    }
+  }
+};
 
 // function to match cards
-const match = function() {};
+const match = function() {
+  openCards[0].classList.add("match");
+  openCards[1].classList.add("match");
 
-// function to close card and hide symbol
-const hide = function() {};
+  openCards[0].classList.remove("close");
+  openCards[1].classList.remove("close");
+};
 
 // function to increament move counter
 const moveCounter = function() {};
@@ -76,56 +112,16 @@ const moveCounter = function() {};
 // function for finished game
 const messageCongrats = function() {};
 
-function gameStart() {
-  // Using event deligation so i add just one click event to the frame holding the cards
-  const cards = document.querySelector(".card_frame");
+// Select the frame holding the cards
+const cards = document.querySelector(".card_frame");
 
-  //   declare and empty array to store clicked cards
-  let openCards = [];
+//   declare and empty array to store clicked cards
+let openCards = [];
 
-  //   loop through the card node list and respond to click events on any card
-  cards.addEventListener("click", function(evt) {
-    const targetCard = evt.target;
-
-    //   wnat to be sure we are clicking on an element that represents a card
-    if (targetCard.nodeName.toLowerCase() === "li") {
-      // want to be sure that the card we are clicking is not having the classes open, show and match
-      if (
-        !targetCard.classList.contains("open") &&
-        !targetCard.classList.contains("show") &&
-        !targetCard.classList.contains("match")
-      ) {
-        // Add clicked card into the openCards array and add classes show, and open to them
-        openCards.push(targetCard);
-        targetCard.classList.add("open", "show");
-
-        //Make sure we can only have a max of two cards in the array to check for a match
-        if (openCards.length === 2) {
-          if (
-            openCards[0].firstChild.className ===
-            openCards[1].firstChild.className
-          ) {
-            console.log("Match!");
-            openCards[0].classList.add("match");
-            openCards[1].classList.add("match");
-
-            openCards[0].classList.remove("close");
-            openCards[1].classList.remove("close");
-
-            openCards = [];
-          } else {
-            setTimeout(function() {
-              for (const card of openCards) {
-                card.classList.remove("show", "open");
-              }
-              openCards = [];
-            }, 500);
-          }
-        }
-      }
-    }
-  });
-}
+//   Using event delegation add click event
+cards.addEventListener("click", function(evt) {
+  const targetCard = evt.target;
+  open(targetCard);
+});
 
 buildGrid();
-gameStart();
